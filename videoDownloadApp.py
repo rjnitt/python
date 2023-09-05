bda75b8317eb1149ffe87fadc5a42291bcfbbafe
Rohit Jain <jainrohit@adobe.com>
2023-09-05 23:43:21 +0530

    update requiremt

--- videoDownloadApp.py
+++ videoDownloadApp.py
@@ -43,7 +43,7 @@ def execute_python_code(user_input):
 
     try:
         # downloading the video
-        video_stream.download(output_path=output_directory, filename=dynamic_filename + ".mp4")
+        video_stream.download(filename=dynamic_filename + ".mp4")
     except Exception as e:
         print("Some Error while downloading: ", str(e))  # to handle exception
 
